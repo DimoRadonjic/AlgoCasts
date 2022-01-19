4b3ea2fc882ae99131848620262cf92022f5ff50
DimoRadonjic <dimo.radonjic2112@gmail.com>
2022-01-19 22:31:05 +0100

    Pyramid Solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,22 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  if (n < 0) {
+    return;
+  }
+  const midpoint = Math.floor((n * 2 - 1) / 2);
+  for (let row = 0; row < n; row++) {
+    let level = '';
+    for (let col = 0; col < n * 2 - 1; col++) {
+      if (midpoint - row <= col && midpoint + row >= col) {
+        level += '#';
+      } else {
+        level += ' ';
+      }
+    }
+    console.log(level);
+  }
+}
 
 module.exports = pyramid;
