1abed83fb6203b5650c5e650be23095ead4b6e31
DimoRadonjic <dimo.radonjic2112@gmail.com>
2022-01-20 16:09:41 +0100

    Vowels Solutions

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,34 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
+
+//Solution 1
+//  let res = 0;
+//  for (let i = 0; i < str.length; i++) {
+//    if (
+//      str[i].toLowerCase() === 'a' ||
+//      str[i].toLowerCase() === 'e' ||
+//      str[i].toLowerCase() === 'i' ||
+//      str[i].toLowerCase() === 'o' ||
+//      str[i].toLowerCase() === 'u'
+//    ) {
+//      res++;
+//    }
+//  }
+//  return res;
+
+// Solution 2
+//  let res = 0;
+//   let vowels = ['a', 'e', 'i', 'o', 'u'];
+//   for (let char of str.toLowerCase()) {
+//     if (vowels.includes(char)) {
+//       res++;
+//     }
+//   }
+//   return res;
